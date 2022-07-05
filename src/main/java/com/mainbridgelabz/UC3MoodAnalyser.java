3e59d78431317bdb70142356df8744b2954382e9
Rampravesh25 <rampraveshmallah@gmail.com>
2022-07-05 23:45:45 +0530

    This is the Test for Mood Analyser

---
+++ src/main/java/com/mainbridgelabz/UC3MoodAnalyser.java
@@ -0,0 +1,50 @@
+package com.mainbridgelabz;
+
+public class UC3MoodAnalyser {
+    String message;
+
+    MoodAnalyser() {
+        this.message = "";
+    }
+
+    MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    String analyseMood() {
+        return analyseMood(message);
+    }
+
+    String analyseMood(String message) {
+
+        String messageToReturn = "";
+
+        try {
+
+            if (message == "") {
+                throw new MoodAnalysisException("Empty Mood");
+            }
+
+            String split[] = message.split(" ");
+            for (String var : split) {
+                if (var.equalsIgnoreCase("Sad")) {
+                    messageToReturn = var;
+                    break;
+                } else if (var.equalsIgnoreCase("Happy")) {
+                    messageToReturn = var;
+                    break;
+                }
+            }
+        } catch (NullPointerException e) {
+            messageToReturn = "Happy";
+        } catch (MoodAnalysisException e) {
+            return e.getMessage();
+        }
+
+        if (messageToReturn.equals("")) {
+            return "Happy";
+        }
+
+        return messageToReturn;
+    }
+}
